be25cd168d76e3d0be731453f0db03a7e8a95898
DESKTOP-QB2CK6H\Carmine <carmine-bnt@hotmail.com>
2021-03-08 15:23:33 +0100

    Stock business created closes #6

---
+++ src/main/java/com/netmind/business/StockBlImpl.java
@@ -0,0 +1,11 @@
+package com.netmind.business;
+
+import com.netmind.business.contracts.StockBl;
+
+public class StockBlImpl implements StockBl {
+
+	public boolean importCsv() {
+		return true;
+	}
+
+}
